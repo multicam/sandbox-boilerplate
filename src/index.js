295bb52541fc37176fe046e8bba3f7f3f166c727
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2018-07-02 06:28:41 +1000

    wip \1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from "react";
+import { render } from "react-dom";
+import App from "./app";
 
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
-
-const App = () => (
-  <div style={styles}>
-    <Hello name="Multi" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
-  </div>
-);
-
-render(<App />, document.getElementById('root'));
+const renderApp = () => render(<App />, document.getElementById("root"));
+renderApp();
