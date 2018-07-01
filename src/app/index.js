db429a5953e5e2c4d11195963cd4832a380dcec6
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2018-07-02 06:32:02 +1000

    wip \2

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hello from "../components/Hello";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+export default () => (
+  <div style={styles}>
+    <Hello name="Multi" />
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+  </div>
+);
